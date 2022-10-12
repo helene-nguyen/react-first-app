589c51cfcb78ceace189314e818bd1dbb375ac32
helene-nguyen <75518128+helene-nguyen@users.noreply.github.com>
2022-10-12 16:06:13 +0000

    Build: (7f0d07b) :rocket: go

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-// Since React 18, we use `createRoot` / Import from react-dom/client !
-import { App } from './App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
